b3ce0d500aaf33e3472030eb247b3347fe12ed5a
Kristin <1358365@DESKTOP-4FH9G41>
2018-12-27 02:20:14 +0300

    final version

--- code.py
+++ code.py
@@ -1,111 +1,82 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Dec 23 17:58:28 2018
+Created on Sun Dec 23 17:58:28 2018
 
-@author: DreamTeam
 """
-#import libraries to work
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
- 
+import xgboost as xgb 
+
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression, LassoCV
+from sklearn import metrics
 
-from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LinearRegression, Lasso, Ridge
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_log_error, mean_squared_error
-from sklearn.preprocessing import PolynomialFeatures
-from sklearn.linear_model import LassoCV, RidgeCV
-from sklearn.preprocessing import RobustScaler
-import warnings
-import xgboost as xgb
-from hyperopt import hp
-from h import fmin, tpe, STATUS_OK, Trials
-
-np.set_printoptions(suppress=True)
-warnings.filterwarnings('ignore')
-
-def rmsle(ypred, ytest): 
-    assert len(ytest) == len(ypred)
-    return np.sqrt(np.mean((np.log1p(ypred) - np.log1p(ytest))**2))
-
-def print_error_info(y_test,predicted):
-    print("RMSE: ", np.sqrt(mean_squared_error(y_test, predicted)))
-    print("RMSLE: ", rmsle(y_test, predicted))
+allowedPercentageOfMissingData = 50
 
 fileName = "train.csv"
-
 rawData = pd.read_csv(fileName, delimiter = ",")
 
 #print(rawData.shape)
 #print(rawData.head())
 #print(rawData.info())
 
-# Function to calculate missing values by column
-def missing_values_table(df):
-        mis_val = df.isnull().sum()
-        mis_val_percent = 100 * df.isnull().sum() / len(df)
+missingValues = rawData.isnull().sum()
+missingValuesPercentage = 100 * rawData.isnull().sum() / len(rawData)
         
-        mis_val_table = pd.concat([mis_val, mis_val_percent], axis=1)
-        mis_val_table_ren_columns = mis_val_table.rename(columns = {0 : 'Missing Values', 1 : '% of Total Values'})
-        mis_val_table_ren_columns = mis_val_table_ren_columns[mis_val_table_ren_columns.iloc[:,1] != 0].sort_values('% of Total Values', ascending=False).round(1)
-       
-        return mis_val_table_ren_columns
-    
-# Get the columns with > 50% missing
-missing_df = missing_values_table(rawData);
-missing_columns = list(missing_df[missing_df['% of Total Values'] > 50].index)
+missingValuesTable = pd.concat([missingValues,missingValuesPercentage], axis=1)
+missingValuesTable = missingValuesTable.rename(columns = {0 : 'Missing Values', 1 : '% of Total Values'})
+missingValuesTable = missingValuesTable[missingValuesTable.iloc[:,1] != 0].sort_values('% of Total Values', ascending=False).round(1)
 
-# Drop the columns
-rawData = rawData.drop(list(missing_columns), axis = 1)
+#print(missingValuesTable)
+
+listOfMissingColumns = list(missingValuesTable[missingValuesTable['% of Total Values'] > allowedPercentageOfMissingData].index)
+rawData = rawData.drop(list(listOfMissingColumns), axis = 1)
 
 for column in rawData.columns:
     if rawData[column].dtype.kind in 'if':
-        rawData[column].fillna(rawData[column].median(), inplace=True)
+        rawData[column].fillna(rawData[column].median(), inplace = True)
     else:
-        rawData[column].fillna(rawData[column].value_counts().idxmax(),inplace=True) 
+        rawData[column].fillna(rawData[column].value_counts().idxmax(), inplace = True) 
 
 target = rawData['SalePrice']
-rawData.drop(['SalePrice'],axis=1,inplace=True)
+rawData.drop(['SalePrice'], axis=1, inplace = True)
 
 rawData = pd.get_dummies(rawData)
 
-X_train, X_test, y_train, y_test = train_test_split(rawData, target, test_size=0.2)
+XTrain, XTest, yTrain, yTest = train_test_split(rawData, target, test_size = 0.2)
 
+#function stealen from https://www.kaggle.com/marknagelberg/rmsle-function
+def rmsle(y_pred, y_test) : 
+    assert len(y_test) == len(y_pred)
+    return np.sqrt(np.mean((np.log(1+y_pred) - np.log(1+y_test))**2))
 
-#LineraRegression
+print('-------LineraRegression-------')
 LR = LinearRegression()
-LR.fit(X_train,y_train)
-predicted = LR.predict(X_test)
+LR.fit(XTrain,yTrain)
+predictedValues = LR.predict(XTest)
 
-print_error_info(predicted,y_test)
+print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(predictedValues, yTest)))
+print('Root Mean Squared Log Error:', rmsle(predictedValues, yTest))
 
-print(LR.coef_.shape)
-print(LR.coef_)
 
-plt.scatter(y_test, predicted)
-
-#LassoRegression
+print('-------LassoRegression-------')
 lasso = LassoCV(alphas=np.logspace(-1,4,25))
+lasso.fit(XTrain,yTrain)
+predictedValues = lasso.predict(XTest)
 
-lasso.fit(X_train,y_train)
+print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(predictedValues, yTest)))
+print('Root Mean Squared Log Error:', rmsle(predictedValues, yTest))
 
-predicted = lasso.predict(X_test)
-print_error_info(predicted, y_test)
 
-print(lasso.coef_.shape)
-
-plt.scatter(y_test, predicted)
-
-#XGBoost
-
+print('-------XGBoost--------')
 model = xgb.XGBRegressor(n_estimators=100, learning_rate=0.08, gamma=0, subsample=0.75,
-                           colsample_bytree=1, max_depth=7)
+                           colsample_bytree=1, max_depth=10)
 
-traindf, testdf = train_test_split(X_train, test_size = 0.3)
-model.fit(X_train, y_train)
-predictions = model.predict(X_test)
-print_error_info(y_test, predictions)
+model.fit(XTrain, yTrain)
+predictedValues = model.predict(XTest)
 
-plt.scatter(y_test, predictions)
+print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(predictedValues, yTest)))
+print('Root Mean Squared Log Error:', rmsle(predictedValues, yTest))
+
+#plt.scatter(yTest, predictions)
